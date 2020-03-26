22e0bd6db750613b6855169e78ab0f03338e57fe
rezaavoor <reza.avoor@gmail.com>
2020-03-26 14:17:54 +0000

    app is able to do all the functions now

--- src/Utils/serverController.js
+++ src/Utils/serverController.js
@@ -3,8 +3,15 @@ require('dotenv').config()
 const BASE_URL = process.env.REACT_APP_SERVER_URL
 
 export const toast = (time) => {
-  fetch(`${BASE_URL}/api/motor/1/10/${time}`).then(res => {
-    console.log(res.statusText)
-  })
-    .catch(e => console.log(e))
+  try {
+    fetch(`${BASE_URL}/api/motor/1/100/2`) // drag down
+    setTimeout(() => {
+      fetch(`${BASE_URL}/api/on/${time}`) // turn it on
+      setTimeout(() => {
+        fetch(`${BASE_URL}/api/motor/1/-60/0.4`) // drag up
+      }, time * 1000)
+    }, 2000)
+  }
+
+  catch (e) { console.log(e) }
 }
